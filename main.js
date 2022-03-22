30038ef65fddef1e9162047d8c742f70f741077c
nvmRegi <cabael.regine@gmail.com>
2022-03-22 14:44:07 +0100

    var

---
+++ main.js
@@ -0,0 +1,10 @@
+const costoKm = 0.21;
+
+const eta = parseInt(prompt("Quanti anni hai?"));
+const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+
+let prezzokm = 0.21*km;
+
+if(eta < 18){
+    
+}
