57f0e277dd42e111104e1f994065ad060fecb621
nvmRegi <cabael.regine@gmail.com>
2022-03-22 15:03:39 +0100

    versione1

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 const costoKm = 0.21;
+let eta;
+let km;
 
-const eta = parseInt(prompt("Quanti anni hai?"));
-const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+do{
+    eta = parseInt(prompt("Quanti anni hai?"));
+} while (eta < 0)
+
+do{
+    km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+} while (km < 0)
 
 let prezzokm = 0.21*km;
-
-if(eta < 18){
-    
+let prezzofin;
+if(eta >= 0 && eta <= 11){
+    alert("Il passeggero viaggia gratis.")
+} else if(eta > 11 && eta < 18){
+    prezzofin = prezzokm - prezzokm * 0.2;
+    alert("Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin);
+} else if(eta > 65){
+    prezzofin = prezzokm - prezzokm * 0.4;
+    alert("Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin);
+} else{
+    prezzofin = prezzokm;
+    alert("Il passeggero dovrà pagare: " + prezzofin);
 }
