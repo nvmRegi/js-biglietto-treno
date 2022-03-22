3599bded707b046c4cf26f2ab64f58dc96006f53
nvmRegi <cabael.regine@gmail.com>
2022-03-22 17:37:44 +0100

    Bonus1.03

--- Bonus/bonus.js
+++ Bonus/bonus.js
@@ -4,5 +4,9 @@ let persona2 = parseInt(prompt("Inserire l'età della seconda persona:"));
 console.log(persona1);
 console.log(persona2);
 if(persona1 > persona2){
-    
+    alert("La prima persona è più grande.");
+} else if (persona1 == persona2){
+    alert("Hanno la stessa età.")
+}else{
+    alert("La seconda persona è più grande.")
 }
