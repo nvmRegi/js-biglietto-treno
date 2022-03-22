48191bf9cdf31e38f47f889c22deedea935665d0
nvmRegi <cabael.regine@gmail.com>
2022-03-22 17:30:09 +0100

    Bonus1.01

---
+++ Bonus/bonus.js
@@ -0,0 +1,8 @@
+let persona1 = parseInt(prompt("Inserire l'età della prima persona:"));
+let persona2 = parseInt(prompt("Inserire l'età della seconda persona:"));
+
+console.log(persona1);
+console.log(persona2);
+if(persona1 > persona2){
+    
+}
