cbfd265c5c31dbdefab4dbc9bac3e01c2dccfb36
nvmRegi <cabael.regine@gmail.com>
2022-03-23 10:22:05 +0100

    correzione2

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const costoKm = 0.21;
 let eta;
 let km;
 
@@ -12,26 +11,35 @@ do{
 
 let prezzokm = 0.21*km;
 let prezzofin;
-if(eta >= 0 && eta <= 11){
+
+if(eta >= 0 && eta <= 11){  //BAMBINI
+
     alert("Il passeggero viaggia gratis.")
     document.getElementById("finale").innerHTML="Il passeggero viaggia gratis.";
-} else if(eta > 11 && eta < 18){
+
+} else if(eta > 11 && eta < 18){    //ADOLESCENTI
+
     prezzofin = Math.round((prezzokm - prezzokm * 0.2 + Number.EPSILON) * 100) / 100;
     alert("Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin + "\u20AC");
     document.getElementById("finale").innerHTML="Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin + "\u20AC";
-    console.log(prezzofin);
-} else if(eta > 65){
+
+} else if(eta >= 65){   //SENIOR
+
     prezzofin = Math.round((prezzokm - prezzokm * 0.4 + Number.EPSILON) * 100) / 100;
     alert("Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin + "\u20AC");
     document.getElementById("finale").innerHTML="Il passeggero ha uno sconto del 20% e dovrà pagare: " + prezzofin + "\u20AC";
 } else{
-    if(prezzofin==null || prezzofin==NaN || prezzofin==undefined){
+
+    if(prezzofin==null || prezzofin==NaN || prezzofin==undefined){ //ERRORE
         alert("ERRORE");
         document.getElementById("finale").innerHTML="ERRORE";
-    }else{
+
+    } else{ //PREZZO NON SCONTATO
+
         prezzofin = prezzokm;
         alert("Il passeggero dovrà pagare: " + prezzofin + "\u20AC");
         document.getElementById("finale").innerHTML="Il passeggero dovrà pagare: " + prezzofin + "\u20AC";
+
     }
-    
+       
 }
